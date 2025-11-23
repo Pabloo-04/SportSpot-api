1be53efb6e493fca35652a88ffeeaf119b7d1b43
robnez44 <00044623@uca.edu.sv>
2025-11-23 00:45:05 -0600

    organizing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js'
+import connectDB from './src/config/mongodb.js'
 import adminGeneralRouter from './routes/adminGeneralRoute.js'
 import canchaRouter from './routes/canchaRoute.js'
 import userRouter from './routes/userRoute.js'
